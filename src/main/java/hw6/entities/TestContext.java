911cd5c42f5224d647a4ba9b1da0ae60736e0ebf
Khodakovsky, Dmitry <Dmitrii.Khodakovsky@Clarivate.com>
2019-07-09 18:45:43 +0300

    Added comments fopr the HW6 #1

--- src/main/java/hw6/entities/TestContext.java
+++ src/main/java/hw6/entities/TestContext.java
@@ -3,6 +3,7 @@ package hw6.entities;
 import hw6.pageObjects.BasePage6;
 import org.openqa.selenium.WebDriver;
 
+// TODO This class should be in the test scope
 public class TestContext {
 
     private static WebDriver driver;
@@ -17,10 +18,12 @@ public class TestContext {
         TestContext.driver = driver;
     }
 
+    // TODO I think this get is not necessary here you could invoke pages using Page.getInstance()....
     public static BasePage6 getActualPage() {
         return actualPage;
     }
 
+    // TODO I think this set is not necessary here you could invoke pages using Page.getInstance()....
     public static void setActualPage(BasePage6 basePage6) {
         actualPage = basePage6;
     }
--- src/main/java/hw6/enums/Labels/BaseLabel.java
+++ src/main/java/hw6/enums/Labels/BaseLabel.java
@@ -1,3 +1,4 @@
+// TODO The package name should be in the low case
 package hw6.enums.Labels;
 
 public interface BaseLabel {
